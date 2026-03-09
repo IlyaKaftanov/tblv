1093d811a8c67a46e4d814b8c5beb39fc7000608
ilia.kaftanov <ilia.kaftanov.personal@gmail.com>
2026-03-09 15:06:38 +0200

    feat: add data layer with Polars lazy loading

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
 mod cli;
+mod data;
 
 use clap::Parser;
 use cli::Cli;
+use data::DataSource;
 
 fn main() -> color_eyre::Result<()> {
     color_eyre::install()?;
     let cli = Cli::parse();
-    println!("File: {:?}, Head: {}", cli.file, cli.head);
+
+    let mut ds = DataSource::open(&cli.file, cli.head)?;
+    let df = ds.head()?;
+    println!("Loaded {} rows x {} cols", df.height(), df.width());
+    println!("Columns: {:?}", ds.column_names()?);
+
     Ok(())
 }
